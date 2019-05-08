cdbd0f5a0fa72f0626844832c396081ea14455aa
Thais Oliveira <borgesoliveira.thais@gmail.com>
2019-05-07 22:53:48 -0300

    feat: changed login style to be the same of register and updated form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { first } from 'rxjs/internal/operators/first';
+import { AuthenticationService } from '@app/_services';
 
 @Component({
 	selector: 'app-login',
-	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.scss']
+	templateUrl: './login.component.html'
 })
 export class LoginComponent implements OnInit {
 
 	public loginForm: FormGroup;
-
-	public user = 'TESTE';
-	public password = 'ARCANUM';
+	public submitted = false;
+	public returnUrl: string;
 
 	constructor(
 		private _formBuilder: FormBuilder,
+		private _route: ActivatedRoute,
 		private _router: Router,
+		private _authenticationService: AuthenticationService,
+		// private _alertService: AlertService
 	) { }
 
 	ngOnInit() {
@@ -25,8 +27,13 @@ export class LoginComponent implements OnInit {
 			username: ['', Validators.required],
 			password: ['', Validators.required]
 		});
+
+
+		// get return url from route parameters or default to '/'
+		this.returnUrl = this._route.snapshot.queryParams['returnUrl'] || '/';
 	}
 
+	// convenience getter for easy access to form fields
 	get f() { return this.loginForm.controls; }
 
 	onSubmit() {
@@ -36,9 +43,16 @@ export class LoginComponent implements OnInit {
 			return;
 		}
 
-		if(this.loginForm.value.username === this.user && this.loginForm.value.password === this.password){
-			this._router.navigate(['dragons']);
-		}
+		this._authenticationService.login(this.f.username.value, this.f.password.value)
+			.pipe(first())
+			.subscribe(
+				data => {
+					this._router.navigate([this.returnUrl]);
+				},
+				error => {
+					// this._alertService.error(error);
+					// this.loading = false;
+				});
 	}
 
 }
